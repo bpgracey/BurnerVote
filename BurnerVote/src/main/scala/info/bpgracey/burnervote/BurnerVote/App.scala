f34e28c9a79050d017a138c332fca3e7240f20fd
Bancroft Gracey <bancroft.gracey@gmail.com>
2016-12-06 16:27:01 -0800

    Initial commit
    Basic Maven archetype plus initial Spray routing & JSON. Tests left as
    default.

---
+++ BurnerVote/src/main/scala/info/bpgracey/burnervote/BurnerVote/App.scala
@@ -0,0 +1,42 @@
+package info.bpgracey.burnervote.BurnerVote
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+import akka.http.scaladsl.server.Directives
+
+/**
+ * @author Bancroft Gracey
+ */
+
+case class BurnerMessage(msgType: String, payload: String, fromNumber: String, toNumber: String, userid: String, burnerId: String)
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val burnerMessageFormat = jsonFormat6(BurnerMessage)
+}
+
+class JsonService extends Directives with JsonSupport {
+  val route =
+    post {
+      path("/event") {
+        entity(as[BurnerMessage]) { message =>
+          complete("Received")
+        }
+      } ~
+     get {
+        path("/report") {
+          complete("Reporting")
+        }
+      }
+  }
+}
+
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
